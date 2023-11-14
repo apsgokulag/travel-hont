b037977cc52c85761b862d5ca01c9632ec88f755
Anglin-George <30459349+Anglin-George@users.noreply.github.com>
2023-11-14 23:56:30 +0530

    WIP : loader in booking page pay checkout

--- resources/views/invoice/layout/app.blade.php
+++ resources/views/invoice/layout/app.blade.php
@@ -4,7 +4,7 @@
 
     <head>
 
-    <title>{{ ucfirst(Route::currentRouteName()); }}</title>
+    <title>{{ config('app.name') }}</title>
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
